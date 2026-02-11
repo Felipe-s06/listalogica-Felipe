3938d4d3335e602e786fbd89365271024b3632e4
Felipe-s06 <luiz.f.pereira29@aluno.senai.br>
2026-02-11 19:59:37 -0300

    Primeira questão

--- Program.cs
+++ Program.cs
@@ -1,7 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
 //Níivel 1: Variáveis e Operações Básicas
 #region 1. Calculadora de média
+Console.WriteLine("Digite a primeira nota:");
+decimal nota1 = Convert.ToDecimal(Console.ReadLine());
+Console.WriteLine("Digite a segunda nota:");
+decimal nota2 = Convert.ToDecimal(Console.ReadLine());
+Console.WriteLine("Digite a terceira nota:");
+decimal nota3 = Convert.ToDecimal(Console.ReadLine());
 
+decimal media = nota1 + nota2 + nota3 / 3;
+
+Console.WriteLine($"A sua média é: {media}.");
 #endregion
 
 #region 2. Conversor de Idade
