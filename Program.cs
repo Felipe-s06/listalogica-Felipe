0331ff35be38dfd6179c8eeb155784cac6e42f78
Felipe-s06 <luiz.f.pereira29@aluno.senai.br>
2026-02-11 19:28:24 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Níivel 1: Variáveis e Operações Básicas
+#region 1. Calculadora de média
+
+#endregion
+
+#region 2. Conversor de Idade
+#endregion
