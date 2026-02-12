53e504b5b4696ac437cfb17831fc9b00a61f3e4e
Felipe-s06 <luiz.f.pereira29@aluno.senai.br>
2026-02-12 20:00:14 -0300

    Terceira e quarta questão

--- Program.cs
+++ Program.cs
@@ -10,18 +10,43 @@
 
 //decimal media = nota1 + nota2 + nota3 / 3;
 
-//Console.WriteLine($"A sua média é: {media}.");
+//Console.WriteLine($"A sua média é: {media:F2}.");
 #endregion
 
 #region 2. Conversor de Idade
-Console.Write($"Digite sua idade: ");
-int idade = Convert.ToInt32(Console.ReadLine());
+//Console.Write($"Digite sua idade: ");
+//int idade = Convert.ToInt32(Console.ReadLine());
 
-int conversaoIdade = idade * 365;
+//int conversaoIdade = idade * 365;
 
-Console.WriteLine($"\nVocê tem {conversaoIdade} dias.");
+//Console.WriteLine($"\nVocê tem {conversaoIdade} dias.");
 #endregion
 
 #region 3. Troca de Valores
+//int Numero1 = 10;
+//int Numero2 = 16;
+//Console.WriteLine($"O primeiro número é {Numero1} e o segundo núemro é {Numero2}.\n");
 
+//Console.Write($"Digite 'Troca' para fazer as trocas do números: ");
+//Console.ReadLine();
+//int Numero3 = Numero1;
+//Numero1 = Numero2;
+//Numero2 = Numero3;
+
+//Console.WriteLine($"\nAgora o primeiro número é {Numero1} e o segundo núemro é {Numero2}.");
+
+#endregion
+
+#region 4. Verificador de Maioridade
+Console.Write("Digite sua idade:");
+int idade = Convert.ToInt32(Console.ReadLine());
+
+if (idade >= 18)
+{
+    Console.WriteLine("Entrada autorizada.");
+}
+else
+{
+    Console.WriteLine("Entrada não autorizada.");
+}
 #endregion
