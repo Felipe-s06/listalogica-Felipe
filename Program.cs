763b58aa610586717005c448d710637bfdf1f929
Felipe-s06 <luiz.f.pereira29@aluno.senai.br>
2026-02-11 21:06:46 -0300

    Segunda questão

--- Program.cs
+++ Program.cs
@@ -1,17 +1,27 @@
 ﻿// See https://aka.ms/new-console-template for more information
 //Níivel 1: Variáveis e Operações Básicas
 #region 1. Calculadora de média
-Console.WriteLine("Digite a primeira nota:");
-decimal nota1 = Convert.ToDecimal(Console.ReadLine());
-Console.WriteLine("Digite a segunda nota:");
-decimal nota2 = Convert.ToDecimal(Console.ReadLine());
-Console.WriteLine("Digite a terceira nota:");
-decimal nota3 = Convert.ToDecimal(Console.ReadLine());
+//Console.Write("Digite a primeira nota: ");
+//decimal nota1 = Convert.ToDecimal(Console.ReadLine());
+//Console.Write("Digite a segunda nota: ");
+//decimal nota2 = Convert.ToDecimal(Console.ReadLine());
+//Console.Write("Digite a terceira nota: ");
+//decimal nota3 = Convert.ToDecimal(Console.ReadLine());
 
-decimal media = nota1 + nota2 + nota3 / 3;
+//decimal media = nota1 + nota2 + nota3 / 3;
 
-Console.WriteLine($"A sua média é: {media}.");
+//Console.WriteLine($"A sua média é: {media}.");
 #endregion
 
 #region 2. Conversor de Idade
+Console.Write($"Digite sua idade: ");
+int idade = Convert.ToInt32(Console.ReadLine());
+
+int conversaoIdade = idade * 365;
+
+Console.WriteLine($"\nVocê tem {conversaoIdade} dias.");
+#endregion
+
+#region 3. Troca de Valores
+
 #endregion
